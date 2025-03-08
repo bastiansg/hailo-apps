46831c54e6376be0758d43ed8f1ae30c563bb202
Bas <bastiansg.bas@gmail.com>
2025-03-08 15:17:37 -0300

    add Servos module

---
+++ hailo_apps/servos/__init__.py
@@ -0,0 +1,1 @@
+from .servos import Servos, ServoAngles  # noqa
---
+++ hailo_apps/servos/servos.py
@@ -0,0 +1,17 @@
+from adafruit_servokit import ServoKit
+from pydantic import BaseModel, NonNegativeInt, Field
+
+
+class ServoAngles(BaseModel):
+    x: NonNegativeInt = Field(le=180, default=103)
+    y: NonNegativeInt = Field(le=180, default=55)
+
+
+class Servos:
+    def __init__(self, init_servo_angles: ServoAngles):
+        self.servo_kit = ServoKit(channels=16)
+        self.set_angles(servo_angles=init_servo_angles)
+
+    def set_angles(self, servo_angles: ServoAngles) -> None:
+        self.servo_kit.servo[0].angle = servo_angles.x
+        self.servo_kit.servo[1].angle = servo_angles.y
