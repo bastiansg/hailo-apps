2c4fc47e2cfc10ffdf8f524e593aefafde9b6605
Bas <bastiansg.bas@gmail.com>
2025-07-23 01:25:37 -0300

    improve PicamApp

--- hailo_apps/meta/interfaces/picam_app.py
+++ hailo_apps/meta/interfaces/picam_app.py
@@ -1,30 +1,20 @@
 import numpy as np
 
 from PIL import Image
-from threading import Thread, Lock
+from threading import Lock, Event
 from abc import ABC, abstractmethod
-from typing import TypeVar, Generic, Callable, Any
+from typing import TypeVar, Generic
 
 from picamera2 import Picamera2
 from pydantic import BaseModel, PositiveInt
 
 from common.utils.path import create_path
+from common.utils.threading import threaded
 
 from .hailo_app import HailoApp
 
 
 T = TypeVar("T", bound="PicamApp")
-F = TypeVar("F", bound=Callable[..., None])
-
-
-# TODO: Move this function to common
-def threaded(func: Callable[..., None]) -> Callable[..., None]:
-    def wrapper(*args: Any, **kwargs: Any) -> None:
-        thread = Thread(target=func, args=args, kwargs=kwargs)
-        thread.daemon = True
-        thread.start()
-
-    return wrapper
 
 
 class ImageSize(BaseModel):
@@ -55,7 +45,7 @@ class PicamApp(HailoApp["PicamApp"], ABC, Generic[T]):
         self.picam = self.get_picam(image_size=image_size)
 
         self.mutex = Lock()
-        self.is_active = False
+        self.stop_event = Event()
 
     def __del__(self):
         self.picam.stop()
@@ -82,26 +72,25 @@ class PicamApp(HailoApp["PicamApp"], ABC, Generic[T]):
 
     @threaded
     def run(self) -> None:
-        self.mutex.acquire()
-        self.picam.start()
+        with self.mutex:
+            self.stop_event.clear()
+            self.picam.start()
 
-        self.is_active = True
-        while self.is_active:
-            np_image = self.picam.capture_array()
-            np_image = np_image[:, :, :3]  # Remove alpha channel.
+            while not self.stop_event.is_set():
+                np_image = self.picam.capture_array()
+                np_image = np_image[:, :, :3]  # Remove alpha channel.
 
-            if self.rotation_180:
-                np_image = np_image[::-1, ::-1]  # 180-degree rotation.
+                if self.rotation_180:
+                    np_image = np_image[::-1, ::-1]  # 180-degree rotation.
 
-            if self.debug_mode:
-                pil_image = Image.fromarray(np_image)
-                pil_image.save(self.debug_image_path)
-                self.is_active = False
+                if self.debug_mode:
+                    pil_image = Image.fromarray(np_image)
+                    pil_image.save(self.debug_image_path)
+                    break
 
-            self.on_frame(np_image=np_image)
+                self.on_frame(np_image=np_image)
 
-        self.picam.stop()
-        self.mutex.release()
+            self.picam.stop()
 
     def stop(self) -> None:
-        self.is_active = False
+        self.stop_event.set()
